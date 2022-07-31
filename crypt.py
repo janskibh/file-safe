9671b8cd0e27ed91e23d8fb15755c98803cf6d9b
Jan Bellon <49980074+janskibh@users.noreply.github.com>
2022-07-31 12:04:45 +0200

    Babiboune

--- crypt.py
+++ crypt.py
@@ -1,60 +1,52 @@
+############################ IMPORTING MODULES ################################
+
 from cryptography.fernet import Fernet
 import os
 import getpass
 from hashlib import scrypt
 from base64 import urlsafe_b64encode
 
-print("\n")
-'''
-password = getpass.getpass("Password : ")
-password_encoded = base64.b64encode(password.encode())
-key = base64.urlsafe_b64encode(password_encoded)
-'''
+###############################################################################
 
-password = getpass.getpass("Password : ")
+
+########################## FERNET KEY GENERATING ##############################
+
+password = getpass.getpass("\nPassword : ")
 salt = b'5\x8b%w\x0f\xb6S\x1fC\xee\x91D\xd2#\x186'
 key = scrypt(password.encode(), salt=salt, n=16384, r=8, p=1, dklen=32)
 key_encoded = urlsafe_b64encode(key)
 
-directory = os.getcwd() + "/files/"
-
-print(directory)
-
-'''
-decoded_key = key.decode()
-
-if not len(decoded_key) == 44:
-    if len(decoded_key) < 44:
-        while not len(decoded_key) == 44:
-            decoded_key = "".join((decoded_key, "="))
-    elif len(key) > 44:
-        while not len(decoded_key) == 44:
-            decoded_key = decoded_key[1:]
-    else:
-        print("Key length incorrect")
-    
-    key = decoded_key.encode()
-'''
-
-print("\nFernet key : " + Fernet.generate_key().decode())
 print("\nFernet key : " + key_encoded.decode() + "\n")
 
 f = Fernet(key_encoded)
 
-if not directory.endswith("files/"):
+###############################################################################
+
+
+############################ FILES ENUMERATION ################################
+
+directory = os.getcwd() + "/"
+
+files_directory = directory + "files/"
+
+if not files_directory.endswith("files/"):
     print("wrong diretory")
     exit()
 
-file_list = os.listdir(directory)
+file_list = os.listdir(files_directory)
 
 for i, file in enumerate(file_list):
-    file_list[i] = directory + file
+    file_list[i] = files_directory + file
+    print(file_list[i])
 
-print(file_list)
+file_list.append(directory + "test_file")
 
-test_token = 'abc'.encode()
+##############################################################################
 
-def encode():
+
+########################## ENCRYPTING FUNCTION ###############################
+
+def encrypt():
     with open(f"{directory}test_file", 'rb') as test:
         test_content = test.read()
         if test_content == test_token:
@@ -71,10 +63,14 @@ def encode():
             print('An error has occured')
             test.close()
             exit()
-    print("files encrypted")
+    print("\nfiles encrypted\n")
         
+##############################################################################
 
-def decode():
+
+######################### DECRYPTING FUNCTION#################################
+
+def decrypt():
     with open(f"{directory}test_file", 'rb') as test:
         if test_content == test_token:
             print("An error has occured")
@@ -95,11 +91,20 @@ def decode():
             else:
                 print("Password incorrect")
                 test.close()
-    print("files decrypted")
+    print("\nfiles decrypted\n")
+
+##############################################################################
+
+
+############## CHOOSING BETWEEN ENCRYPT AND DECRYPT FUNCTION #################
+
+test_token = 'abc'.encode()
 
 with open(f"{directory}test_file", "rb") as test_file:
     test_content = test_file.read()
     if test_content == test_token:
-        encode()
+        encrypt()
     else:
-        decode()
+        decrypt()
+
+##############################################################################
